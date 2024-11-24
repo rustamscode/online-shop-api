1b604f1d363f018d8a9791701b36b1de8b4ca8c1
Agagyl <agagylrustamov@gmail.com>
2024-11-24 11:08:09 +0400

    Initial commit

---
+++ src/main/java/rustamscode/onlineshopapi/OnlineShopApiApplication.java
@@ -0,0 +1,13 @@
+package rustamscode.onlineshopapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineShopApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineShopApiApplication.class, args);
+    }
+
+}

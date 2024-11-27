2ef22ec5de6566c9cdf3fcb8dc1be68d863773b8
Agagyl <agagylrustamov@gmail.com>
2024-11-27 23:20:15 +0400

    Bug fix

--- src/main/java/rustamscode/onlineshopapi/controller/GlobalExceptionHandler.java
+++ src/main/java/rustamscode/onlineshopapi/controller/GlobalExceptionHandler.java
@@ -1,12 +1,12 @@
 package rustamscode.onlineshopapi.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springdoc.api.ErrorMessage;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import rustamscode.onlineshopapi.exception.*;
+import rustamscode.onlineshopapi.model.ErrorMessage;
 
 @RestControllerAdvice
 @Slf4j
